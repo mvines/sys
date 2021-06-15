4a8bbd30324481dcef0c569c932136380ed56ec7
Michael Vines <mvines@gmail.com>
2021-06-15 15:25:23 -0700

    Allow activating state as well

--- src/rpc_client_utils.rs
+++ src/rpc_client_utils.rs
@@ -10,12 +10,18 @@ pub fn get_stake_authorized(
 ) -> Result<(Authorized, Pubkey), Box<dyn std::error::Error>> {
     let stake_account = rpc_client.get_account(&stake_account_address)?;
 
-    if rpc_client
+    match rpc_client
         .get_stake_activation(stake_account_address, None)?
         .state
-        != StakeActivationState::Active
     {
-        return Err(format!("Stake account must be active: {}", stake_account_address).into());
+        StakeActivationState::Active | StakeActivationState::Activating => {}
+        state => {
+            return Err(format!(
+                "Stake account {} must be Active or Activating: {:?}",
+                stake_account_address, state
+            )
+            .into());
+        }
     }
 
     match stake_account.state() {
